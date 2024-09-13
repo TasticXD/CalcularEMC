a2d478fa4e343f0b26cef5ebc0dc1f72aca8467d
TasticXD <162758302+TasticXD@users.noreply.github.com>
2024-09-13 14:55:55 -0300

    App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './telas/LoginScreen';
-import CalculatorScreen from './telas/CalculatorScreen';
+import Calculadora from './telas/Calculadora';
+import Inicial from './telas/Inicial';
 
 const Stack = createStackNavigator();
 
@@ -10,8 +10,8 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} options={({ navigation }) => ({ navigation })} />
-        <Stack.Screen name="Calculator" component={CalculatorScreen} />
+        <Stack.Screen name="Inicial" component={Inicial} options={({ navigation }) => ({ navigation })} />
+        <Stack.Screen name="Calculadora" component={Calculadora} />
       </Stack.Navigator>
     </NavigationContainer>
   );
